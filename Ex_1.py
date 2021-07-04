dbc1b20381c2cf06e8c36a22284e79165fbede6c
PhamMinhHieu-rvc <minhhieupham98@gmail.com>
2021-07-04 20:24:31 +0700

    Fist Commit

---
+++ Ex_1.py
@@ -0,0 +1,27 @@
+from sklearn import tree
+
+#buoc 1: Thu thập dữ liệu
+#buoc 2: Xử lí dữ liệu
+#buoc 3: Training model
+#buoc 4: Dự đoán kết quản
+# Bước đánh giá model
+
+my_tree = tree.DecisionTreeClassifier()
+
+
+dactrung = [[1, 3, 3, 7],
+            [5, 2, 4, 6],
+            [1, 2, 4, 6],
+            [5, 4, 4, 3],
+            [1, 4, 4, 7],
+            [3, 2, 3, 7],
+            [3, 3, 3, 6],
+            [5, 2, 2, 7],
+            ]
+
+nhan = [0 ,1, 1, 0, 0, 0, 0, 1]
+
+result = my_tree.fit(dactrung, nhan)
+
+ketqua =  result.predict([[1, 3 ,4, 7]])
+print(ketqua)
